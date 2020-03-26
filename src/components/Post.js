97dd6df436313cd8c7f029ae8a339d2f8c0aa21e
Volkan Ongun <me@volkanongun.com>
2020-03-26 17:04:29 +0300

    modals added

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Quote from './Quote'
 import Photo from './Photo'
 import Link from './Link'
@@ -8,47 +8,105 @@ import RegularPost from './RegularPost'
 
 const Post = (props) => {
 
-	function prettyDate(d){
+	const [showDetailsModal, setShowDetailsModal] = useState(false);
 
-		let split = d.split(" ");
-		let newDate = split[0].split("-")[1] + " " + split[0].split("-")[2] + " " + split[0].split("-")[0]
-
-		return newDate;
+	const handleClose = function(){
+		setShowDetailsModal(false);
 	}
+
+	const handleClick = function(id){
+		setShowDetailsModal(true);
+	}
+
+	function contentSwitcher(type){
+		switch(type) {
+		    case 'quote':
+		      return <div class="modal-content">
+		      			<Quote props={props}></Quote>
+		      		</div>;
+		    case 'photo':
+		      return <div class="modal-content">
+		      			<Photo props={props}></Photo>
+		      		</div>;
+		    case 'link':
+		      return <div class="modal-content">
+		      			<Link props={props}></Link>
+		      		</div>;
+		    case 'conversation':
+		      return <div class="modal-content">
+		      			<Conversation props={props}></Conversation>
+		      		</div>;
+		    case 'audio':
+		      return <div class="modal-content">
+		      			<Audio props={props}></Audio>
+		      		</div>;
+		    default:
+		      return <div class="modal-content">
+		      			<RegularPost props={props}></RegularPost>
+		      		</div>;
+		  	}
+	}
+
+	const DetailsModal = ({ handleClose, item, show }) => {
+		const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+		console.log(item, " π");
+
+	 	return (
+		    <div className={showHideClassName}>
+
+		    	<div className="modal-holder">
+
+		    		<button onClick={handleClose} className="btn btn-sm btn-primary close-btn"><strong>X</strong> close</button>
+
+		    		<div className="overflow details">
+						{contentSwitcher(item.type)}
+				    </div>
+		    	</div>
+			       
+		    </div>
+		);
+	};
 
 	function renderSwitch(type) {
 	  switch(type) {
 	    case 'quote':
 	      return <div>
 	      			<Quote props={props}></Quote>
-	      			<div className="btn-holder"><button className="btn btn-primary">See Details</button></div>
+	      			<DetailsModal item={props} show={showDetailsModal} handleClose={handleClose}></DetailsModal>
+	      			<div className="btn-holder"><button className="btn btn-primary" onClick={() => handleClick()}>See Details</button></div>
 	      		</div>;
 	    case 'photo':
 	      return <div>
 	      			<Photo props={props}></Photo>
-	      			<div className="btn-holder"><button className="btn btn-primary">See Details</button></div>
+	      			<DetailsModal item={props} show={showDetailsModal} handleClose={handleClose}></DetailsModal>
+	      			<div className="btn-holder"><button className="btn btn-primary" onClick={() => handleClick()}>See Details</button></div>
 	      		</div>;
 	    case 'link':
 	      return <div>
 	      			<Link props={props}></Link>
-	      			<div className="btn-holder"><button className="btn btn-primary">See Details</button></div>
+	      			<DetailsModal item={props} show={showDetailsModal} handleClose={handleClose}></DetailsModal>
+	      			<div className="btn-holder"><button className="btn btn-primary" onClick={() => handleClick()}>See Details</button></div>
 	      		</div>;
 	    case 'conversation':
 	      return <div>
 	      			<Conversation props={props}></Conversation>
-	      			<div className="btn-holder"><button className="btn btn-primary">See Details</button></div>
+	      			<DetailsModal item={props} show={showDetailsModal} handleClose={handleClose}></DetailsModal>
+	      			<div className="btn-holder"><button className="btn btn-primary" onClick={() => handleClick()}>See Details</button></div>
 	      		</div>;
 	    case 'audio':
 	      return <div>
 	      			<Audio props={props}></Audio>
-					<div className="btn-holder"><button className="btn btn-primary">See Details</button></div>
+	      			<DetailsModal item={props} show={showDetailsModal} handleClose={handleClose}></DetailsModal>
+					<div className="btn-holder"><button className="btn btn-primary" onClick={() => handleClick()}>See Details</button></div>
 	      		</div>;
 	    default:
 	      return <div>
 	      			<RegularPost props={props}></RegularPost>
-	      			<div className="btn-holder"><button className="btn btn-primary">See Details</button></div>
+	      			<DetailsModal item={props} show={showDetailsModal} handleClose={handleClose}></DetailsModal>
+	      			<div className="btn-holder"><button className="btn btn-primary" onClick={() => handleClick()}>See Details</button></div>
 	      		</div>;
-	  }
+	  	}
 	}
 
 	return (
